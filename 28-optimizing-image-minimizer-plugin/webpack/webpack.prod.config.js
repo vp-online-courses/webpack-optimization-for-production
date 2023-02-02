fa875acf4d2de6c116183150b632fc5b0c8457c6
Viktor Pyskunov <vpyskunov@gmail.com>
2023-02-02 12:26:26 +0200

    28-optimizing-image-minimizer-plugin: ImageMinimizerWebpackPlugin

---
+++ 28-optimizing-image-minimizer-plugin/webpack/webpack.prod.config.js
@@ -0,0 +1,122 @@
+const common = require('./webpack.common.config.js')
+const { merge } = require('webpack-merge') 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const path = require('path')
+const glob = require('glob')
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: 'js/[name].[contenthash:12].js'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin({
+                minimizerOptions: {
+                    preset: [
+                        'default',
+                        {
+                            discardComments: { removeAll: true },
+                        },
+                    ],
+                },
+            }),
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        // Lossless optimization with custom option
+                        // Feel free to experiment with options for better result for you
+                        plugins: [
+                            ['imagemin-mozjpeg', { quality: 40 }],
+                            ['imagemin-pngquant', {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            }],
+                            ['imagemin-gifsicle', { interlaced: true }],
+                            [
+                                'imagemin-svgo',
+                                {
+                                    plugins: [
+                                        {
+                                            name: 'preset-default',
+                                            params: {
+                                                overrides: {
+                                                    removeViewBox: false,
+                                                    addAttributesToSVGElement: {
+                                                        params: {
+                                                            attributes: [
+                                                                { xmlns: 'http://www.w3.org/2000/svg' },
+                                                            ],
+                                                        },
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    ],
+                                },
+                            ],
+                        ],
+                    },
+                },
+            }),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+            },
+            {
+                test: /\.css$/,
+                include: /\.module\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[hash:base64]'
+                            }
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.less$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'less-loader' ],
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader' ],
+            },
+            {
+                test: /\.(png|jpg|svg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024 // 10 kb
+                    }
+                },
+                generator: {
+                    filename: './images/[name].[contenthash:12][ext]'
+                },
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:12].css',
+        }),
+        new PurgeCSSPlugin({
+            paths: glob.sync(`${path.join(__dirname, '../src')}/**/*`,  { nodir: true }),
+        })
+    ]
+})
